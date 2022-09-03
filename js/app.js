a9d16c17a788ac5eff2eeef8a683af8b005d7317
Anita-Mahmud <anita.mahmud13@gmail.com>
2022-09-04 02:06:10 +0600

    added category list

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-// const loadNews = () => {
-//     fetch('')
-// }
+const loadNewsData = () => {
+    fetch('https://openapi.programming-hero.com/api/news/categories')
+        .then(res => res.json())
+        .then(data => displayCatList(data.data.news_category))
+}
+const displayCatList = category_lists => {
+    console.log(category_lists)
+    const categoryLists = document.getElementById('category-name-list');
+    category_lists.forEach(cat_list => {
+        const li = document.createElement('li');
+        li.classList.add('p-4')
+        li.innerHTML = `
+        <a onclick>${cat_list.category_name}</a>
+        `
+        categoryLists.appendChild(li);
+    })
+
+}
+loadNewsData()
